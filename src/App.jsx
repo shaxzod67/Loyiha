1c0589ae56dce6df931f50cf1cdc0c93335927b8
shaxzod67 <uzbuz741@gmail.com>
2024-06-14 19:43:41 +0500

    new loyiha

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Header from './Header/header'
+
+function App() {
+
+  return (
+   <div className="contaner">
+      <Header/>
+   </div>
+  )
+}
+
+export default App
